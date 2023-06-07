0b1276de54275855261a87d86d287db15701da2b
Matbobe <matcanos1@gmail.com>
2023-06-07 16:37:27 +0200

    rename des sequentiels

--- src/main/java/com/pictoseq/controllers/EditController.java
+++ src/main/java/com/pictoseq/controllers/EditController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -24,6 +26,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.http.HttpClient;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 public class EditController {
@@ -64,6 +67,7 @@ public class EditController {
     void initialize() {
         client = HttpClient.newHttpClient();
         searchList = new SearchList(searchListGrid, client, this);
+
     }
     @FXML
     void handleTextSearch(KeyEvent event) {
@@ -111,6 +115,64 @@ public class EditController {
         searchList.renderNext();
     }
 
+    private void addNameViewSequenciel(){
+        HBox container = new HBox();
+        Text nameSequentiel = new Text();
+
+        nameSequentiel.setText(sequentiel.getName());
+        nameSequentiel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold; -fx-fill: black;");
+        container.getChildren().add(nameSequentiel);
+
+        Button renameSequentiel = new Button("Renommer");
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/pencilW-icon.png")));
+        renameSequentiel.setLayoutX(10);
+        renameSequentiel.setLayoutY(10);
+        renameSequentiel.setStyle("-fx-font-size: 12px; -fx-font-weight: bold; -fx-background-color:#3056D3; -fx-text-fill: White");
+        renameSequentiel.setGraphic(new ImageView(image));
+        renameSequentiel.setOnAction(event -> {
+            headerBox.getChildren().remove(0);
+            HBox container1 = new HBox();
+            headerBox.getChildren().add(container1);
+            TextField textField = new TextField(nameSequentiel.getText());
+
+            container1.getChildren().add(textField);
+            container1.setAlignment(javafx.geometry.Pos.CENTER_LEFT);
+            container1.setSpacing(10);
+            textField.requestFocus();
+            textField.end();
+            textField.setStyle("-fx-border-width: 0; -fx-border-color: transparent; -fx-background-color: transparent;-fx-font-weight: bold; -fx-text-fill: black; -fx-font-size: 24px;");
+
+
+            Button Confirme = new Button("Valider");
+            Confirme.setLayoutX(10);
+            Confirme.setLayoutY(10);
+            Confirme.setStyle("-fx-font-size: 12px; -fx-font-weight: bold; -fx-background-color:#3056D3; -fx-text-fill: White");
+            Confirme.setOnAction(event1 -> {
+              sequentiel.setName(textField.getText());
+              headerBox.getChildren().clear();
+              addNameViewSequenciel();
+            });
+            Button Cancel = new Button("Annuler");
+            Cancel.setLayoutX(10);
+            Cancel.setLayoutY(10);
+            Cancel.setStyle("-fx-font-size: 12px; -fx-font-weight: bold; -fx-background-color:White; -fx-text-fill: #3056D3");
+            Cancel.setOnAction(event1 -> {
+                headerBox.getChildren().clear();
+                addNameViewSequenciel();
+            });
+            container1.getChildren().addAll(Confirme, Cancel);
+
+        });
+        container.getChildren().add(renameSequentiel);
+        container.setAlignment(javafx.geometry.Pos.CENTER_LEFT);
+        container.setSpacing(10);
+
+        headerBox.getChildren().add(container);
+
+
+
+    }
+
     public void addPictogramme(Pictograme pictograme) {
         sequentiel.addPictograme(pictograme);
         Log.println("Added pictogram to the sequentiel");
@@ -119,6 +181,7 @@ public class EditController {
         this.sequentiel = sequentiel;
         scrollPaneSequentiel.setContent(sequentiel.getVboxSequentiel());
         scrollPaneSequentiel.setPannable(true);
+        addNameViewSequenciel();
     }
 
     public void setColor(ColorPicker idColor){
